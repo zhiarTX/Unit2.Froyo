4c32eedc9dda3d32865885912e9d01e001cde287
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-09-21 17:59:45 -0600

    Update index.js
    Changed prompt and created a command to have the flavors be counted each time the users inputs the item.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const aFlavor = window.prompt(`Type in your flavor!!`);
+const userInputString = prompt(
+    "Please enter some flavors seperated by commas.", "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
+
+const stringArray = userInputString.split(",");
+const flavors = {};
+
+for (const flavor of stringArray){
+    if (flavor in flavors){
+        flavors[flavor].push(flavor);
+    }
+    else {
+      flavors[flavor] = [flavor];
+    }
+  }
+
+let flavorString =``;
+for (const [flavor, count] of Object.entries(flavors)){
+    flavorString += `${flavor} ${count},`;
+}
+console.log(flavorString);
